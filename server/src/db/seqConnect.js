1ecf7ad274e725909279d65ed15fbcea76044acd
Yassine JOUT <yassinejout@gmail.com>
2020-11-14 15:51:50 +0100

    refactor(db): use orm sequelize

---
+++ server/src/db/seqConnect.js
@@ -0,0 +1,24 @@
+import Sequelize from "sequelize";
+import conf from "dotenv";
+conf.config();
+
+// define connetion object
+export const db = new Sequelize(
+  process.env.PGDATABASE,
+  process.env.PGUSER,
+  process.env.PGPASSWORD,
+  {
+    host: process.env.PGHOST,
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      aqcuire: 30000,
+      idle: 10000,
+    },
+  }
+);
+// connect to the db
+export const connect = async () => {
+  return await db.authenticate();
+};
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { getRisksFromDb } from "./repositories/Risk.repository"
+import { getRisksFromDb } from "./repositories/Risk.repository";
+import { connect } from "./db/seqConnect";
+// db Connect
+connect();
 
 let PORT = 8080;
 var app = express();
@@ -9,7 +12,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/",async (req, res) => {
+app.get("/", async (req, res) => {
   const result = await getRisksFromDb();
   res.send(result);
 });
